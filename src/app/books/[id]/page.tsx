60f78c08a4a3fbb15d53b07e1132a0210b8d8ac5
carl-pixelbuilders <carl@pixelbuilders.com>
2025-01-22 08:35:43 +0000

    Updating styling of sections in on book details page

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -2,7 +2,7 @@ import {
     BookDetails,
     BookDescription,
     BookReviews,
-    BookAuthor
+    BookAuthor,
 } from "@/components/pages/book";
 import { getBook } from "@/server/actions/books";
 import { getAuthor } from "@/server/actions/authors";
@@ -27,24 +27,33 @@ function BookNotFound() {
 }
 
 export default async function BookPage({ params }: { params: { id: string } }) {
-
     const { data: book, error: bookError } = await getBook(params.id);
-
 
     if (!book || !book.authorId) {
         return <BookNotFound />;
     }
 
-    const { data: reviews, error: reviewsError } = await getReviewsByBookId(book.id);
+    const { data: reviews, error: reviewsError } = await getReviewsByBookId(
+        book.id
+    );
 
-    const { data: author, error: authorsError } = await getAuthor(book.authorId);
-
-    const rating = reviews.reduce((acc, review) => acc + review.rating, 0) / reviews.length;
+    const { data: author, error: authorsError } = await getAuthor(
+        book.authorId
+    );
+    const rating =
+        reviews.length > 0
+            ? reviews.reduce((acc, review) => acc + review.rating, 0) /
+              reviews.length
+            : undefined;
 
     return (
         <div className="container pt-10 pb-20 flex flex-col gap-10 lg:grid lg:grid-cols-[400px_minmax(0,60ch)] lg:gap-10 lg:mx-auto lg:w-fit">
             <div className="lg:sticky lg:top-10">
-                <BookDetails book={book} author={author || undefined} rating={rating} />
+                <BookDetails
+                    book={book}
+                    author={author || undefined}
+                    rating={rating}
+                />
             </div>
             <div className="flex flex-col gap-10">
                 <BookDescription book={book} />
@@ -52,9 +61,7 @@ export default async function BookPage({ params }: { params: { id: string } }) {
                 <BookReviews reviews={reviews} />
                 <hr />
                 {author && <BookAuthor author={author} />}
-
             </div>
         </div>
     );
 }
-
--- src/components/pages/book.tsx
+++ src/components/pages/book.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -13,7 +13,6 @@ import { getHumanReadableDate } from "@/lib/utils";
 import { ImageOff, StarIcon } from "lucide-react";
 
 import Avatar from "boring-avatars";
-import { Children } from "react";
 
 function BookActions() {
     return (
@@ -25,116 +24,184 @@ function BookActions() {
                 Leave a review
             </Button>
         </div>
-    )
+    );
 }
 
-function BookGenres({ genres }: { genres: string[] }) {
-    return (
-        <div className="flex flex-wrap gap-2 justify-center">
-            {genres.map((genre) => (
-                <Button asChild variant={"outline"} key={genre} size={"sm"}>
-                    <Link href={`/books?genre=${genre}`}>
-                        {genre}
-                    </Link>
-                </Button>
-            ))}
-        </div>
-    )
-}
-
-function BookHeader({ title, author, children }: { title: string, author?: Author, children?: React.ReactNode }) {
+function BookHeader({
+    title,
+    author,
+    children,
+}: {
+    title: string;
+    author?: Author;
+    children?: React.ReactNode;
+}) {
     return (
         <div className="flex flex-col gap-2 items-center px-6 py-4 rounded-tl-lg rounded-br-lg rounded-tr-3xl rounded-bl-3xl bg-secondary text-secondary-foreground w-full">
             <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-serif lg:text-4xl font-bold">
                     {title}
                 </h1>
-                {author ? <Link href={`/authors/${author.id}`} className="text-sm lg:text-base text-foreground/50 hover:underline">
-                    {author.name}
-                </Link> : <p className="text-sm lg:text-base text-gray-500">Unknown Author</p>}
+                {author ? (
+                    <Link
+                        href={`/authors/${author.id}`}
+                        className="text-sm lg:text-base text-foreground/50 hover:underline"
+                    >
+                        {author.name}
+                    </Link>
+                ) : (
+                    <p className="text-sm lg:text-base text-gray-500">
+                        Unknown Author
+                    </p>
+                )}
             </div>
             {children}
         </div>
-    )
+    );
 }
 
-function BookDetails({ book, author, rating }: { book: Book, author?: Author, rating?: number }) {
+function BookDetails({
+    book,
+    author,
+    rating,
+}: {
+    book: Book;
+    author?: Author;
+    rating?: number;
+}) {
     return (
-        <StickyBlock stickyMode="desktop" className="flex flex-col items-center gap-10" >
+        <StickyBlock
+            stickyMode="desktop"
+            className="flex flex-col items-center gap-10"
+        >
             <div className="flex flex-col gap-2 items-center lg:items-start">
                 <BookHeader title={book.title} author={author}>
-                    {rating && <span className="flex items-center gap-1 text-xs"><StarIcon className={`w-3 h-3 text-transparent fill-yellow-500`} />{rating.toFixed(1)}</span>}
+                    {rating && (
+                        <span className="flex items-center gap-1 text-xs">
+                            <StarIcon
+                                className={`w-3 h-3 text-transparent fill-`}
+                            />
+                            {rating.toFixed(1)}
+                        </span>
+                    )}
                 </BookHeader>
-                {book.genres && <BookGenres genres={book.genres} />}
             </div>
-            {book.coverImage ? <Image src={book.coverImage} alt={book.title} width={200} height={300} className="rounded-lg" /> : (
+            {book.coverImage ? (
+                <Image
+                    src={book.coverImage}
+                    alt={book.title}
+                    width={200}
+                    height={300}
+                    className="rounded-lg"
+                />
+            ) : (
                 <div className="relative rounded-lg bg-secondary flex items-center justify-center aspect-[2/3] w-[200px]">
-                    <ImageOff size={80} className="text-gray-500/5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-                    <p className="text-lg font-bold text-gray-500 text-center">No cover image available</p>
+                    <ImageOff
+                        size={80}
+                        className="text-gray-500/5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    />
+                    <p className="text-lg font-bold text-gray-500 text-center">
+                        No cover image available
+                    </p>
                 </div>
             )}
             <BookActions />
         </StickyBlock>
-    )
+    );
 }
 
 function BookDescription({ book }: { book: Book }) {
     return (
         <div className="flex flex-col gap-4">
-            <h2 className="text-2xl font-serif font-bold">Publisher Description</h2>
-            <div className="text-lg text-gray-500">{book.description}</div>
+            <h2 className="text-lg font-serif">Publisher Description</h2>
+            <div className="text-foreground">{book.description}</div>
         </div>
-    )
+    );
 }
 
 function BookReviews({ reviews }: { reviews: Review[] }) {
-    const averageRating = reviews.length > 0 ? reviews.reduce((acc, review) => acc + review.rating, 0) / reviews.length : 0;
+    const averageRating =
+        reviews.length > 0
+            ? reviews.reduce((acc, review) => acc + review.rating, 0) /
+              reviews.length
+            : 0;
     return (
         <div className="flex flex-col gap-5">
             <div className="flex justify-between gap-4">
-                <h2 className="text-2xl font-serif font-bold">{reviews.length} Review{reviews.length > 1 ? "s" : ""}</h2>
-                {reviews.length > 0 ? <span className="flex items-center gap-2 text-foreground/50"><RatingStars rating={averageRating} size="md" />{averageRating.toFixed(1)} </span> : <div className="text-lg text-gray-500">No reviews yet</div>}
-
+                <h2 className="text-lg font-serif">
+                    {reviews.length} Review
+                    {reviews.length > 1 || reviews.length === 0 ? "s" : ""}
+                </h2>
+                {reviews.length > 0 ? (
+                    <span className="flex items-center gap-2 text-foreground/50">
+                        <RatingStars rating={averageRating} size="md" />
+                        {averageRating.toFixed(1)}{" "}
+                    </span>
+                ) : (
+                    <div className="text-lg text-gray-500">No reviews yet</div>
+                )}
             </div>
             <div className="flex flex-col gap-5">
-                {reviews.length > 0 ? reviews.map((review) => (
-                    <div key={review.id} className="last:border-b-0 border-b border-primary/10 pb-5 last:pb-0">
-                        <div className="flex flex-col gap-2">
-                            <div className="flex items-center gap-3">
-                                <Avatar name={review.userId} size={32} variant="beam" />
-                                <div className="flex flex-col">
-                                    <span className="text-lg font-bold font-serif">{review.userId}</span>
-                                    <span className="text-sm text-gray-500">{getHumanReadableDate(review.datePosted)}</span>
-
+                {reviews.length > 0 ? (
+                    reviews.map((review) => (
+                        <div
+                            key={review.id}
+                            className="last:border-b-0 border-b border-primary/10 pb-5 last:pb-0"
+                        >
+                            <div className="flex flex-col gap-2">
+                                <div className="flex items-center gap-3">
+                                    <Avatar
+                                        name={review.userId}
+                                        size={32}
+                                        variant="beam"
+                                    />
+                                    <div className="flex flex-col">
+                                        <span className="text-lg font-bold font-serif">
+                                            {review.userId}
+                                        </span>
+                                        <span className="text-sm text-gray-500">
+                                            {getHumanReadableDate(
+                                                review.datePosted
+                                            )}
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="rounded-lg bg-secondary p-4 flex flex-col gap-2">
-                                <RatingStars rating={review.rating} size="sm" />
-                                <div className="text-lg text-secondary-foreground">
-                                    <p>{review.comment}</p>
+                                <div className="rounded-lg bg-secondary p-4 flex flex-col gap-2">
+                                    <RatingStars
+                                        rating={review.rating}
+                                        size="sm"
+                                    />
+                                    <div className="text-secondary-foreground">
+                                        <p>{review.comment}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )) : <div className="text-lg text-gray-500">No reviews yet</div>}
+                    ))
+                ) : (
+                    <div className="text-lg text-gray-500">No reviews yet</div>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
 function BookAuthor({ author }: { author: Author }) {
     return (
         <div className="flex flex-col gap-4">
-            <h2 className="text-xl font-serif font-bold">About {author.name}</h2>
-            {author.bio && <div className="text-lg text-gray-500">{author.bio}</div>}
+            <h2 className="text-lg font-serif">About {author.name}</h2>
+            {author.bio ? (
+                <div className="text-foreground">{author.bio}</div>
+            ) : (
+                <div className="text-foreground/50">No bio available</div>
+            )}
+            <div className="flex justify-start mt-6">
+                <Button asChild variant={"outline"} size={"lg"}>
+                    <Link href={`/authors/${author.id}`}>View profile</Link>
+                </Button>
+            </div>
         </div>
-    )
+    );
 }
 
-
-export {
-    BookDetails,
-    BookDescription,
-    BookReviews,
-    BookAuthor
-}
+export { BookDetails, BookDescription, BookReviews, BookAuthor };
