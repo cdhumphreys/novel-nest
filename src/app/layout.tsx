db86a69c8080a6792da2dc37ffd04f4ea6a5bc3f
Carl humphreys <humphreys.carl.d@gmail.com>
2025-01-24 16:46:36 +0000

    Adding hydration warning suppression for ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className + " min-h-screen flex flex-col"}>
         <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
           <Header />
